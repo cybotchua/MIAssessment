cfa2424a14f9f6d534a0438a1cccc81e8e90f363
Terence Chua <cybotchua@hotmail.com>
2019-05-16 15:43:16 +0800

    Edit timezone name

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import HomePage from './components/HomePage';
 import { Header } from './components/common';
+import moment from 'moment-timezone';
 
 class App extends Component {
     render() {
+        var timeZone = moment.tz.guess();
+
         return (
             <View style={{ flex: 1 }}>
-              <Header headerText="Singapore, Singapore" />
+              <Header headerText={timeZone} />
               <HomePage />
             </View>
         );
